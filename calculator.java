dc8ef6c2cbde67cb722a1d9b17a5eab0baeeaa75
tej <tejaswark2006@gmail.com>
2026-09-17 14:32:05 +0530

    commited

--- calculator.java
+++ calculator.java
@@ -3,47 +3,12 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        double a = 10;
+        double b = 5;
 
-        System.out.print("Enter first number: ");
-        double a = sc.nextDouble();
-
-        System.out.print("Enter operator (+, -, *, /): ");
-        char op = sc.next().charAt(0);
-
-        System.out.print("Enter second number: ");
-        double b = sc.nextDouble();
-
-        double result;
-
-        switch (op) {
-            case '+':
-                result = a + b;
-                break;
-
-            case '-':
-                result = a - b;
-                break;
-
-            case '*':
-                result = a * b;
-                break;
-
-            case '/':
-                if (b == 0) {
-                    System.out.println("Cannot divide by zero!");
-                    return;
-                }
-                result = a / b;
-                break;
-
-            default:
-                System.out.println("Invalid operator!");
-                return;
-        }
-
-        System.out.println("Result = " + result);
-
-        sc.close();
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
     }
 }
